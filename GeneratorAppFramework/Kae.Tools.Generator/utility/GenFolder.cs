9e2ce3372080139df8892c7b82b28817a10128d9
kae-made <93808496+kae-made@users.noreply.github.com>
2022-08-05 11:00:42 +0900

    Add create backup file feature for GenFolder
    Code set for https://www.nuget.org/packages/Kae.Tools.Generator/3.3.0

--- GeneratorAppFramework/Kae.Tools.Generator/utility/GenFolder.cs
+++ GeneratorAppFramework/Kae.Tools.Generator/utility/GenFolder.cs
@@ -10,21 +10,71 @@ namespace Kae.Tools.Generator.utility
 {
     public class GenFolder
     {
-        public string BaseFolder { get; set; }
-        public async Task WriteContentAsync(string folderName, string fileName, string content, bool overwrite = true)
+        public enum WriteMode
         {
-            bool store = true;
+            Overwrite,
+            None,
+            Backup
+        }
+        public string BaseFolder { get; set; }
+
+        [Obsolete("Please use other signature")]
+        public async Task WriteContentAsync(string folderName, string fileName, string content,bool store = true)
+        {
+            var mode = WriteMode.None;
+            if (store) mode = WriteMode.Overwrite;
+            await WriteContentAsync(folderName, fileName, content, mode);
+        }
+
+        public async Task WriteContentAsync(string folderName, string fileName, string content, WriteMode writeMode = WriteMode.Overwrite)
+        {
+            WriteMode store = WriteMode.Overwrite;
             var absolutePath = Path.Join(BaseFolder, folderName, fileName);
             if (File.Exists(absolutePath))
             {
-                store = overwrite;
-                if (store)
+                store = writeMode;
+                if (store == WriteMode.Overwrite)
                 {
                     File.Delete(absolutePath);
                 }
+                else if (store == WriteMode.Backup)
+                {
+                    string backupExt = "bak";
+                    int backNo = 0;
+                    var dirInfo = new DirectoryInfo(folderName);
+                    foreach (var file in dirInfo.GetFiles())
+                    {
+                        if (file.Name.StartsWith(fileName))
+                        {
+                            var frags = file.Name.Split(new char[] { '.' });
+                            if (frags.Length == 3)
+                            {
+                                if (backNo == 0)
+                                {
+                                    backNo = 1;
+                                }
+                                string bNo = frags[2].Substring(backupExt.Length);
+                                if (bNo.Length > 0)
+                                {
+                                    int bIndex = int.Parse(bNo);
+                                    if (backNo <= bIndex)
+                                    {
+                                        backNo = bIndex +1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    string backupFileName = $"{fileName}.{backupExt}";
+                    if (backNo > 0)
+                    {
+                        backupFileName = $"{backupFileName}{backNo}";
+                    }
+                    File.Move(absolutePath, Path.Join(folderName, backupFileName));
+                }
             }
 
-            if (store)
+            if (store== WriteMode.Overwrite||store== WriteMode.Backup)
             {
                 using (var writer = File.CreateText(absolutePath))
                 {
